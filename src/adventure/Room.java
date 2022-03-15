d080bfbbd5dae08d3e15a1b1dd9dcb7370c94789
AsHansen88 <Ande970s@stud.kea.dk>
2022-03-15 15:29:31 +0100

    Adventure Del 1 add rum, help, exit, player + moving player.

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -2,37 +2,74 @@ package adventure;
 
 public class Room {
 
+  private Room North, South, West, East;
+  private String Name;
+  private String Description;
 
-  public void One() {
 
-  }
-
-  public void Two() {
-
-  }
-  public void Tree() {
-
-  }
-
-  public void Four() {
-
-  }
-  public void Five() {
+  void Room(String Roomname, String RoomDescription) {
+    setName(Roomname);
+    setDescription(RoomDescription);
 
   }
 
-  public void Six() {
+
+  public Room getNorth(){
+    return North;
+  }
+  public void SetNorth(Room aNorth) {
+    this.North = aNorth;
 
   }
-  public void Seven() {
+  public Room getSouth(){
+    return South;
+  }
+  public void setSouth(Room aSouth) {
+    this.South = aSouth;
+
+  }
+  public Room getEast() {
+    return East;
+  }
+  public void setEast(Room aEast) {
+    this.East = aEast;
+ }
+  public Room getWest() {
+
+    return West;
+  }
+  public void setWest(Room aWest) {
+    this.West = aWest;
 
   }
 
-  public void Eight() {
+  public Room (String aName, String aDescription) {
+
+    this.Name = aName;
+    this.Description = aDescription;
 
   }
 
-  public void Nine() {
+  public String getName() {
+    return Name;
+  }
 
+  public void setName(String aName) {
+    this.Name = aName;
+  }
+  public String getDescription() {
+    return Description;
+  }
+  public void setDescription(String aDescription) {
+    this.Description = aDescription;
+
+  }
+
+  @Override
+  public String toString() {
+    return "Room{" +
+        "Name='" + Name + '\'' +
+        ", Description='" + Description + '\'' +
+        '}';
   }
 }
